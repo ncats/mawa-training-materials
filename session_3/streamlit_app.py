e5401362a210466062f3a463855d8b0e06aa6981
Andrew Weisman <andrew.weisman@nih.gov>
2024-10-28 11:09:08 -0400

    Ensure widget values are persisted when switching pages

--- session_3/streamlit_app.py
+++ session_3/streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit as st
 from plot_slide import main as plot_slide
 from plot_distribution import main as plot_distribution
 
+for key in st.session_state.keys():
+    st.session_state[key] = st.session_state[key]
+
 pg = st.navigation([
     st.Page(plot_slide, title="Plot slide", url_path="plot-slide"),
     st.Page(plot_distribution, title="Plot distribution", url_path="plot-distribution"),
